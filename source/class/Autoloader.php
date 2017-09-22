ba23a64c03256c0826b3b52758a7de5e8f32ec33
Julien Delsescaux <julien.delsescaux@clickandboat.com>
2017-09-22 14:26:32 +0200

    synchro

--- source/class/Autoloader.php
+++ source/class/Autoloader.php
@@ -13,27 +13,7 @@ class Autoloader
     public function addNamespace($namespace, $folder)
     {
         $this->namespaces[$namespace] = $folder;
-
-
-        $folder = normalizeFilepath($folder);
-        if (is_dir($folder)) {
-
-
-            $dir_iterator = new \RecursiveDirectoryIterator($folder);
-
-            $iterator = new \RecursiveIteratorIterator($dir_iterator, \RecursiveIteratorIterator::SELF_FIRST);
-
-            foreach ($iterator as $file) {
-                if (strrpos($file, '.php')) {
-
-                    $fileName = str_replace('\\', '/', (string)$file);
-                    $className = filepathToClassName(str_replace($folder, $namespace, $fileName));
-                    $this->classIndex[strtolower($className)] = (string)$file;
-                }
-            }
-        }
-
-
+        $this->generateNamespaceClassIndex($namespace, $folder);
         return $this;
     }
 
@@ -44,31 +24,49 @@ class Autoloader
 
         if (!$this->classIndex) {
             foreach ($this->namespaces as $namespace => $folder) {
-
-                $folder = normalizeFilepath($folder);
-
-                if (is_dir($folder)) {
-                    $dir_iterator = new \RecursiveDirectoryIterator($folder);
-                    $iterator = new \RecursiveIteratorIterator($dir_iterator, \RecursiveIteratorIterator::SELF_FIRST);
-
-                    foreach ($iterator as $file) {
-                        if (strrpos($file, '.php')) {
-
-                            $fileName = str_replace('\\', '/', (string)$file);
-                            $className = filepathToClassName(str_replace($folder, $namespace, $fileName));
-                            $this->classIndex[strtolower($className)] = (string)$file;
-                        }
-                    }
-                }
+                $this->generateNamespaceClassIndex($namespace, $folder);
             }
         }
 
         $normalizedClassName = strtolower($calledClassName);
-
 
         if (isset($this->classIndex[$normalizedClassName])) {
             include($this->classIndex[$normalizedClassName]);
         }
     }
 
+    public function register()
+    {
+        spl_autoload_register(function ($calledClassName) {
+            $this->autoload($calledClassName);
+        });
+    }
+
+
+    protected function generateNamespaceClassIndex($namespace, $folder)
+    {
+        $folder = normalizeFilepath($folder);
+
+        if (is_dir($folder)) {
+            $dir_iterator = new \RecursiveDirectoryIterator($folder);
+            $iterator = new \RecursiveIteratorIterator($dir_iterator, \RecursiveIteratorIterator::SELF_FIRST);
+
+            foreach ($iterator as $file) {
+                if (strrpos($file, '.php')) {
+
+                    $fileName = str_replace('\\', '/', (string)$file);
+                    $className = filepathToClassName(str_replace($folder, $namespace, $fileName));
+                    $parts = explode('\\', $className);
+
+                    $size = count($parts);
+                    if($parts[$size - 1] == $parts[$size - 2]) {
+                        array_pop($parts);
+                        $className = implode('\\', $parts);
+                    }
+
+                    $this->classIndex[strtolower($className)] = (string)$file;
+                }
+            }
+        }
+    }
 }
